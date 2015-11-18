435e1ec1144833c85c027c43f357257982a0c840
Francesco Zanini <francesco@zanini.me>
2015-11-18 18:37:13 +0100

    Add license info to all files, README and LICENSE.txt

--- firmwares/build.go
+++ firmwares/build.go
@@ -1,3 +1,19 @@
 package firmwares
 
+/* Planet Updater - easily update your Elos Planet lamps.
+Copyright (C) 2015 Francesco Zanini <francesco@zanini.me>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
 //go:generate go-bindata -o firmwares.go -pkg firmwares -nomemcopy .
